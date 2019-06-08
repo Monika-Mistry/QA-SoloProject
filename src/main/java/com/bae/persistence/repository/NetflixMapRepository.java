e95d71464a701aa4e796954c749ecbd819b1e410
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-08 15:45:36 +0100

    netflixmaprepo added

---
+++ src/main/java/com/bae/persistence/repository/NetflixMapRepository.java
@@ -0,0 +1,31 @@
+package com.bae.persistence.repository;
+
+public class NetflixMapRepository implements NetflixRepository {
+
+	public String addAProgram(String program) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAllProgrammes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAProgram(String program) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateAProgram(int id, String program) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String removeAProgram(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

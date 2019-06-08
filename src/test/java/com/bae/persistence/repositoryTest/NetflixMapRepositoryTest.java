c055d18c71c5c27fc410115279c9aaa58eed4def
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-08 15:51:04 +0100

    Added JSONUtil class

--- src/test/java/com/bae/persistence/repositoryTest/NetflixMapRepositoryTest.java
+++ src/test/java/com/bae/persistence/repositoryTest/NetflixMapRepositoryTest.java
@@ -1,5 +1,17 @@
 package com.bae.persistence.repositoryTest;
 
+import org.junit.Before;
+
+import com.bae.persistence.repository.NetflixMapRepository;
+
 public class NetflixMapRepositoryTest {
 
+	private NetflixMapRepository nmr;
+	private JSONUtil json;
+
+	@Before
+	public void setup() {
+		nmr = new NetflixMapRepository();
+		json = newJSONUtil();
+	}
 }

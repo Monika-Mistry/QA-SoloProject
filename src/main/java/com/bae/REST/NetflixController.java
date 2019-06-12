402c33e133bc9b8efc8428839a12e44f93f50364
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-12 20:37:05 +0100

    controller annotations added

--- src/main/java/com/bae/REST/NetflixController.java
+++ src/main/java/com/bae/REST/NetflixController.java
@@ -1,7 +1,13 @@
 package com.bae.REST;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import com.bae.business.service.NetflixService;
 
@@ -11,23 +17,38 @@ public class NetflixController {
 	@Inject
 	private NetflixService service;
 
+	@Path("addAProgram")
+	@POST
+	@Produces({ "application/json" })
 	public String addAProgram(String program) {
 		return service.addAProgram(program);
 	}
 
+	@Path("getAllProgrammes")
+	@GET
+	@Produces({ "application/json" })
 	public String getAllProgrammes() {
 		return service.getAllProgrammes();
 	}
 
-	public String getAProgram(int id) {
+	@Path("getAProgram/{id}")
+	@GET
+	@Produces({ "application/json" })
+	public String getAProgram(@PathParam("id") int id) {
 		return service.getAProgram(id);
 	}
 
-	public String updateAProgram(int id, String program) {
+	@Path("updateAProgram/{id}")
+	@PUT
+	@Produces({ "application/json" })
+	public String updateAProgram(@PathParam("id") int id, String program) {
 		return service.updateAProgram(id, program);
 	}
 
-	public String removeAProgram(int id) {
+	@Path("removeAProgram/{id}")
+	@DELETE
+	@Produces({ "application/json" })
+	public String removeAProgram(@PathParam("id") int id) {
 		return service.removeAProgram(id);
 	}
 

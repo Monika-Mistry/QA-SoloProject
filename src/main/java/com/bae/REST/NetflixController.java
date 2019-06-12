af4ff7128db9a341972826cd212a3c0d5c4ad01a
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-12 20:32:22 +0100

    netflix controller methods added

--- src/main/java/com/bae/REST/NetflixController.java
+++ src/main/java/com/bae/REST/NetflixController.java
@@ -1,8 +1,35 @@
 package com.bae.REST;
 
+import javax.inject.Inject;
 import javax.ws.rs.Path;
+
+import com.bae.business.service.NetflixService;
 
 @Path("netflix/")
 public class NetflixController {
+
+	@Inject
+	private NetflixService service;
+
+	public String addAProgram(String program) {
+		return null;
+
+	}
+
+	public String getAllProgrammes() {
+		return null;
+	}
+
+	public String getAProgram(int id) {
+		return null;
+	}
+
+	public String updateAProgram(int id, String program) {
+		return null;
+	}
+
+	public String removeAProgram(int id) {
+		return null;
+	}
 
 }

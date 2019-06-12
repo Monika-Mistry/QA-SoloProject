bbfe7675d1c385294baae1248ad3c3f9a91d7e85
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-12 20:24:28 +0100

    netflixserviceimpl added

---
+++ src/main/java/com/bae/business/service/NetflixServiceImpl.java
@@ -0,0 +1,30 @@
+package com.bae.business.service;
+
+public class NetflixServiceImpl implements NetflixService {
+
+	public String addAProgram(String program) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAllProgrammes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAProgram(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateAProgram(int id, String program) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String removeAProgram(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

c262f0f8479a0c0c699d52c3be2c87457f14b3c9
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-12 17:33:19 +0100

    getAllProgrammes() written

--- src/main/java/com/bae/persistence/repository/NetflixDatabaseRepository.java
+++ src/main/java/com/bae/persistence/repository/NetflixDatabaseRepository.java
@@ -1,9 +1,27 @@
 package com.bae.persistence.repository;
 
+import java.util.Collection;
+
 import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+import com.bae.persistence.domain.Netflix;
+import com.bae.util.JSONUtil;
 
 @Default
+@Transactional(TxType.SUPPORTS)
 public class NetflixDatabaseRepository implements NetflixRepository {
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+
+	@Inject
+	private JSONUtil jsonUtil;
 
 	public String addAProgram(String program) {
 		// TODO Auto-generated method stub
@@ -11,8 +29,11 @@ public class NetflixDatabaseRepository implements NetflixRepository {
 	}
 
 	public String getAllProgrammes() {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = manager.createQuery("SELECT n FROM netflix n");
+
+		Collection<Netflix> programmes = (Collection<Netflix>) query
+				.getResultList();
+		return jsonUtil.getJSONForObject(programmes);
 	}
 
 	public String getAProgram(int id) {

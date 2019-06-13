c7dbabe8d50fdc2be338672e8d0acb23c1019ae2
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-13 17:30:13 +0100

    query constant change to Netflix

--- src/main/java/com/bae/util/Constants.java
+++ src/main/java/com/bae/util/Constants.java
@@ -3,6 +3,6 @@ package com.bae.util;
 public class Constants {
 
 	public static final String PROGRAMNOTEXIST = "{\"message\": \"Program does not exist\"}";
-	public static final String GETALLPROGRAMSQUERY = "SELECT n FROM netflix n";
+	public static final String GETALLPROGRAMSQUERY = "SELECT n FROM Netflix n";
 
 }

328e91dfa79ef55dcd355f1008f804813dec0c13
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-13 17:41:25 +0100

    import.sql updated

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Netflix(netflixId, country, title, genreId) VALUES(1, 'UK', 'Friends', 1);
+INSERT INTO Netflix(netflixId, country, title, genreId) VALUES(2, 'UK', 'Big Bang Theory', 1);
+INSERT INTO Netflix(netflixId, country, title, genreId) VALUES(3, 'USA', 'Brooklyn-Nine-Nine', 1);

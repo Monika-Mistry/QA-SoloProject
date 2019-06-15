5abd62f739253316659ba3f2a333d96b581273d4
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-15 10:57:15 +0100

    changes

--- src/main/java/com/bae/persistence/repository/GenreMapRepository.java
+++ src/main/java/com/bae/persistence/repository/GenreMapRepository.java
@@ -2,13 +2,11 @@ package com.bae.persistence.repository;
 
 public class GenreMapRepository implements GenreRepository {
 
-	@Override
 	public String getAllGenres() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String getAGenre() {
 		// TODO Auto-generated method stub
 		return null;

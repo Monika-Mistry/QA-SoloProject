659179dc2b3bc97f7ba547c1bd670dbd819d1dff
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-15 11:20:22 +0100

    watchlistrepo implementations added

---
+++ src/main/java/com/bae/persistence/repository/WatchlistMapRepository.java
@@ -0,0 +1,29 @@
+package com.bae.persistence.repository;
+
+public class WatchlistMapRepository implements WatchlistRepository {
+
+	@Override
+	public String addAProgram(String program) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getWatchlist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String removeAProgram(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateWatchStatus(int id, String status) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

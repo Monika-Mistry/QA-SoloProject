3247d7b3bb64ff7aaf5c0065a752f88e77272e80
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-15 11:13:44 +0100

    getter/setters and constructors added

--- src/main/java/com/bae/persistence/domain/Watchlist.java
+++ src/main/java/com/bae/persistence/domain/Watchlist.java
@@ -2,10 +2,38 @@ package com.bae.persistence.domain;
 
 import javax.persistence.Entity;
 
+import com.bae.util.WatchStatus;
+
 @Entity
 public class Watchlist {
 
 	// private int userId; //later functionality
 	private int netflixId;
 	private WatchStatus status;
+
+	public Watchlist() {
+
+	}
+
+	public Watchlist(int netflixId, WatchStatus status) {
+		super();
+		this.netflixId = netflixId;
+		this.status = status;
+	}
+
+	public int getNetflixId() {
+		return netflixId;
+	}
+
+	public void setNetflixId(int netflixId) {
+		this.netflixId = netflixId;
+	}
+
+	public WatchStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(WatchStatus status) {
+		this.status = status;
+	}
 }

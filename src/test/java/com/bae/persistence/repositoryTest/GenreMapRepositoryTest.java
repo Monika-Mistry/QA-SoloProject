992406be7eb017935b6ea4bc1f7c59386fba4266
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-15 10:45:38 +0100

    tests method added

--- src/test/java/com/bae/persistence/repositoryTest/GenreMapRepositoryTest.java
+++ src/test/java/com/bae/persistence/repositoryTest/GenreMapRepositoryTest.java
@@ -1,5 +1,34 @@
 package com.bae.persistence.repositoryTest;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class GenreMapRepositoryTest {
 
+	@Before
+	public void setup() {
+
+	}
+
+	@Test
+	public void returnNoGenresWhenMapEmpty() {
+		fail("TODO");
+	}
+
+	@Test
+	public void returnGenresWhenMapFilled() {
+		fail("TODO");
+	}
+
+	@Test
+	public void returnGenreWhenItDoesNotExist() {
+		fail("TODO");
+	}
+
+	@Test
+	public void returnGenreWhenItExists() {
+		fail("TODO");
+	}
 }

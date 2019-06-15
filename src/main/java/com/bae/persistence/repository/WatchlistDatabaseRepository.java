c2c0a3a75fb545d04badec9fc0faf333cf53ef93
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-15 20:22:27 +0100

    setting up watchlistDBrepo

--- src/main/java/com/bae/persistence/repository/WatchlistDatabaseRepository.java
+++ src/main/java/com/bae/persistence/repository/WatchlistDatabaseRepository.java
@@ -1,13 +1,25 @@
 package com.bae.persistence.repository;
 
 import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
+
+import com.bae.util.JSONUtil;
 
 @Default
 @Transactional(TxType.SUPPORTS)
 public class WatchlistDatabaseRepository implements WatchlistRepository {
 
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+
+	@Inject
+	private JSONUtil jsonUtil;
+
+	@Transactional(TxType.REQUIRED)
 	public String addAProgram(String program) {
 		// TODO Auto-generated method stub
 		return null;
@@ -18,11 +30,13 @@ public class WatchlistDatabaseRepository implements WatchlistRepository {
 		return null;
 	}
 
+	@Transactional(TxType.REQUIRED)
 	public String removeAProgram(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(TxType.REQUIRED)
 	public String updateWatchStatus(int id, String status) {
 		// TODO Auto-generated method stub
 		return null;

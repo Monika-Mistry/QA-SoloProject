afd60b4092a12703851d44ed9cd204f137563ddf
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-15 20:32:28 +0100

    service implementation added

---
+++ src/main/java/com/bae/business/service/WatchlistServiceImpl.java
@@ -0,0 +1,25 @@
+package com.bae.business.service;
+
+public class WatchlistServiceImpl implements WatchlistService {
+
+	public String addAProgram(String program) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getWatchlist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String removeAProgram(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateWatchStatus(int id, String status) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

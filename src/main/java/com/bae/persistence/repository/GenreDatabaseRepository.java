4bda43203148d66297f5aad415f3e5fbade9012e
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-15 10:48:15 +0100

    genrerepo implementations added

---
+++ src/main/java/com/bae/persistence/repository/GenreDatabaseRepository.java
@@ -0,0 +1,17 @@
+package com.bae.persistence.repository;
+
+public class GenreDatabaseRepository implements GenreRepository {
+
+	@Override
+	public String getAllGenres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAGenre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

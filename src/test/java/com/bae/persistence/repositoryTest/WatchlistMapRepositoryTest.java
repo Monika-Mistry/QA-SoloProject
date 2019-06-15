3b1830321d7094e6b720f93be9275a9b06398d47
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-15 11:25:16 +0100

    test method added

--- src/test/java/com/bae/persistence/repositoryTest/WatchlistMapRepositoryTest.java
+++ src/test/java/com/bae/persistence/repositoryTest/WatchlistMapRepositoryTest.java
@@ -1,5 +1,60 @@
 package com.bae.persistence.repositoryTest;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class WatchlistMapRepositoryTest {
+
+	@Before
+	public void setup() {
+
+	}
+
+	@Test
+	public void returnNoProgrammesWhenMapEmpty() {
+		fail("TODO");
+	}
+
+	@Test
+	public void returnProgrammesWhenMapFilled() {
+		fail("TODO");
+	}
+
+	@Test
+	public void addAProgramToWL() {
+		fail("TODO");
+	}
+
+	@Test
+	public void addTwoProgrammesToWL() {
+		fail("TODO");
+	}
+
+	@Test
+	public void addProgramAlreadyInWL() {
+		fail("TODO");
+	}
+
+	@Test
+	public void removeProgramFromWL() {
+		fail("TODO");
+	}
+
+	@Test
+	public void removeTwoProgrammesFromWL() {
+		fail("TODO");
+	}
+
+	@Test
+	public void removeProgramNotInWL() {
+		fail("TODO");
+	}
+
+	@Test
+	public void updateWatchStatus() {
+		fail("TODO");
+	}
 
 }

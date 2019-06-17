29bfa2ffa5f153bc09b7b13c382ce5246038e0d4
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-17 14:28:44 +0100

    @RunWith

--- src/test/java/com/bae/persistence/repositoryTest/NetflixDatabaseRepositoryTest.java
+++ src/test/java/com/bae/persistence/repositoryTest/NetflixDatabaseRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.bae.persistence.repositoryTest;
 
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import com.bae.persistence.repository.NetflixDatabaseRepository;
 
+@RunWith(MockitoJUnitRunner.class)
 public class NetflixDatabaseRepositoryTest {
 	
 	private NetflixDatabaseRepository ndbr;

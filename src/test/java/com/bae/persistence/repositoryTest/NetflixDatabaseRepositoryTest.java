01dbd707c5def617a588bf7d845747f06a15982b
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-17 14:26:28 +0100

    setting up NDBR mockito test

---
+++ src/test/java/com/bae/persistence/repositoryTest/NetflixDatabaseRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.bae.persistence.repositoryTest;
+
+import com.bae.persistence.repository.NetflixDatabaseRepository;
+
+public class NetflixDatabaseRepositoryTest {
+	
+	private NetflixDatabaseRepository ndbr;
+
+}

f1a73e9446447f0a0efb08aba2b1447850f8b0a0
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-17 14:34:56 +0100

    setting up mockito test

--- src/test/java/com/bae/persistence/repositoryTest/NetflixDatabaseRepositoryTest.java
+++ src/test/java/com/bae/persistence/repositoryTest/NetflixDatabaseRepositoryTest.java
@@ -1,13 +1,37 @@
 package com.bae.persistence.repositoryTest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.bae.persistence.repository.NetflixDatabaseRepository;
+import com.bae.util.JSONUtil;
 
 @RunWith(MockitoJUnitRunner.class)
 public class NetflixDatabaseRepositoryTest {
 	
+	@InjectMocks
 	private NetflixDatabaseRepository ndbr;
+	
+	@Mock
+	private EntityManager manager;
+	
+	private JSONUtil jsonUtil;
+	
+	@Mock
+	private Query query;
+	
+	@Before
+	public void setup() {
+		ndbr.setManager(manager);
+		jsonUtil = new JSONUtil();
+		ndbr.setUtil(jsonUtil);
+	}
+	
 
 }

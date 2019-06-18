974552da470b3a7093f22b4daf672d46fc74552d
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-18 20:47:28 +0100

    updating import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,11 @@
 INSERT INTO Netflix(netflixId, country, title, genreId) VALUES(1, 'UK', 'Friends', 1);
 INSERT INTO Netflix(netflixId, country, title, genreId) VALUES(2, 'UK', 'Big Bang Theory', 1);
 INSERT INTO Netflix(netflixId, country, title, genreId) VALUES(3, 'USA', 'Brooklyn-Nine-Nine', 1);
+
+INSERT INTO Genre(genreId, genre) VALUES(1, 'Comedy')
+INSERT INTO Genre(genreId, genre) VALUES(2, 'Drama')
+INSERT INTO Genre(genreId, genre) VALUES(3, 'Action')
+INSERT INTO Genre(genreId, genre) VALUES(4, 'Thriller')
+INSERT INTO Genre(genreId, genre) VALUES(5, 'Romance')
+INSERT INTO Genre(genreId, genre) VALUES(6, 'Documentary')
+INSERT INTO Genre(genreId, genre) VALUES(7, 'Originals')

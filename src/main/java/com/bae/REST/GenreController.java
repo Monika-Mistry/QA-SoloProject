bc4c2f1dbf9736ab45debcbf2e2dab04d311f8fa
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-18 20:44:58 +0100

    controller methods written

--- src/main/java/com/bae/REST/GenreController.java
+++ src/main/java/com/bae/REST/GenreController.java
@@ -1,7 +1,10 @@
 package com.bae.REST;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import com.bae.business.service.GenreService;
 
@@ -11,13 +14,19 @@ public class GenreController {
 	@Inject
 	private GenreService service;
 
+	@Path("getAllGenres")
+	@GET
+	@Produces({ "application/json" })
 	public String getAllGenres() {
-		return null;
+		return service.getAllGenres();
 
 	}
 
-	public String getAGenre(int id) {
-		return null;
+	@Path("getAGenre/{id}")
+	@GET
+	@Produces({ "application/json" })
+	public String getAGenre(@PathParam("id") int id) {
+		return service.getAGenre(id);
 
 	}
 }

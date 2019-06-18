c1a83df658a4c4715df236c73be245226aa03116
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-18 20:24:47 +0100

    getallgenres in GDBR written

--- src/main/java/com/bae/persistence/repository/GenreDatabaseRepository.java
+++ src/main/java/com/bae/persistence/repository/GenreDatabaseRepository.java
@@ -1,12 +1,16 @@
 package com.bae.persistence.repository;
 
+import java.util.List;
+
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
+import com.bae.persistence.domain.Genre;
 import com.bae.util.JSONUtil;
 
 @Default
@@ -20,7 +24,9 @@ public class GenreDatabaseRepository implements GenreRepository {
 	private JSONUtil jsonUtil;
 
 	public String getAllGenres() {
-		return null;
+		Query query = manager.createQuery("SELECT g FROM Genre g");
+		List<Genre> genres = query.getResultList();
+		return jsonUtil.getJSONForObject(genres);
 	}
 
 	public String getAGenre(int id) {

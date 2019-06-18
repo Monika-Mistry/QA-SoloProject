4f60f99d0e63b3206705c519aa736241a340861e
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-18 20:03:06 +0100

    adding genreMap

--- src/main/java/com/bae/persistence/repository/GenreMapRepository.java
+++ src/main/java/com/bae/persistence/repository/GenreMapRepository.java
@@ -1,6 +1,13 @@
 package com.bae.persistence.repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.bae.persistence.domain.Genre;
+
 public class GenreMapRepository implements GenreRepository {
+
+	private Map<Integer, Genre> genreMap = new HashMap<>();
 
 	public String getAllGenres() {
 		// TODO Auto-generated method stub
@@ -10,6 +17,10 @@ public class GenreMapRepository implements GenreRepository {
 	public String getAGenre() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public Map<Integer, Genre> getGenreMap() {
+		return genreMap;
 	}
 
 }

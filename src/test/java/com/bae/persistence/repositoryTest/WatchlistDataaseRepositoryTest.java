d0a740d047fefd25b41beda456a3606007582a9d
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-18 21:18:37 +0100

    get tests written

--- src/test/java/com/bae/persistence/repositoryTest/WatchlistDataaseRepositoryTest.java
+++ src/test/java/com/bae/persistence/repositoryTest/WatchlistDataaseRepositoryTest.java
@@ -1,6 +1,10 @@
 package com.bae.persistence.repositoryTest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -10,10 +14,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.bae.persistence.domain.Watchlist;
 import com.bae.persistence.repository.WatchlistDatabaseRepository;
 import com.bae.util.JSONUtil;
+import com.bae.util.TestConstants;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WatchlistDataaseRepositoryTest {
@@ -39,12 +46,22 @@ public class WatchlistDataaseRepositoryTest {
 
 	@Test
 	public void returnNoProgrammesWhenDBEmpty() {
-		fail("TODO");
+		Mockito.when(manager.createQuery(Mockito.anyString()))
+				.thenReturn(query);
+		List<Watchlist> watchlist = new ArrayList<>();
+		Mockito.when(query.getResultList()).thenReturn(watchlist);
+		assertEquals(TestConstants.EMPTYLIST, wdbr.getWatchlist());
+
 	}
 
 	@Test
 	public void returnProgrammesWhenDBIsFilled() {
-		fail("TODO");
+		Mockito.when(manager.createQuery(Mockito.anyString()))
+				.thenReturn(query);
+		List<Watchlist> watchlist = new ArrayList<>();
+		watchlist.add(TestConstants.TEST_WATCHLIST1);
+		Mockito.when(query.getResultList()).thenReturn(watchlist);
+		assertEquals(TestConstants.EMPTYLIST, wdbr.getWatchlist());
 	}
 
 	@Test

bd09dacfbc34cc7d788f6e527ff19acc4f9d5bf6
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-18 20:21:48 +0100

    CDI annotations added

--- src/main/java/com/bae/persistence/repository/GenreDatabaseRepository.java
+++ src/main/java/com/bae/persistence/repository/GenreDatabaseRepository.java
@@ -1,5 +1,11 @@
 package com.bae.persistence.repository;
 
+import javax.enterprise.inject.Default;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+@Default
+@Transactional(TxType.SUPPORTS)
 public class GenreDatabaseRepository implements GenreRepository {
 
 	@Override
--- src/main/java/com/bae/persistence/repository/GenreMapRepository.java
+++ src/main/java/com/bae/persistence/repository/GenreMapRepository.java
@@ -3,10 +3,13 @@ package com.bae.persistence.repository;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.enterprise.inject.Alternative;
+
 import com.bae.persistence.domain.Genre;
 import com.bae.util.Constants;
 import com.bae.util.JSONUtil;
 
+@Alternative
 public class GenreMapRepository implements GenreRepository {
 
 	private Map<Integer, Genre> genreMap = new HashMap<>();

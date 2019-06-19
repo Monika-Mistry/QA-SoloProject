9d7cd71d02878b92e1fcd99678c8640dbd35b611
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-19 22:14:50 +0100

    updated import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,11 +3,11 @@
 --INSERT INTO Netflix(netflixId, country, title, genreId) VALUES(3, 'USA', 'Brooklyn-Nine-Nine', 1);
 
 INSERT INTO Genre(genreId, genreName) VALUES(1, 'Comedy')
-INSERT INTO Genre(genreId, genre) VALUES(2, 'Drama')
-INSERT INTO Genre(genreId, genre) VALUES(3, 'Action')
-INSERT INTO Genre(genreId, genre) VALUES(4, 'Thriller')
-INSERT INTO Genre(genreId, genre) VALUES(5, 'Romance')
-INSERT INTO Genre(genreId, genre) VALUES(6, 'Documentary')
-INSERT INTO Genre(genreId, genre) VALUES(7, 'Originals')
+INSERT INTO Genre(genreId, genreName) VALUES(2, 'Drama')
+INSERT INTO Genre(genreId, genreName) VALUES(3, 'Action')
+INSERT INTO Genre(genreId, genreName) VALUES(4, 'Thriller')
+INSERT INTO Genre(genreId, genreName) VALUES(5, 'Romance')
+INSERT INTO Genre(genreId, genreName) VALUES(6, 'Documentary')
+INSERT INTO Genre(genreId, genreName) VALUES(7, 'Originals')
 INSERT INTO Genre(genreId, genreName) VALUES(9, 'Fantasy')
 INSERT INTO Genre(genreId, genreName) VALUES(10, 'Horror')

171e164432a9ec8d11e7b4b029e333813cc793c3
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-19 11:58:40 +0100

    updated front end url

--- src/main/webapp/scripts/populateDropDown.js
+++ src/main/webapp/scripts/populateDropDown.js
@@ -1,36 +1,55 @@
-let results = [];
+const getAllGenre = "http://34.90.182.15:8888/netflixWatchlistApp/api/genre/getAllGenres";
+const getAllTitle = "http://34.90.182.15:8888/netflixWatchlistApp/api/netflix/getAllProgrammes";
+
 const getTitles = () => {
     makeRequest(
         "GET",
-        "http://localhost:8080/netflixWatchlistApp/api/netflix/getAllProgrammes"
+        getAllTitle
     )
         .then(response => {
             populateTitles(response);
         })
         .catch(error => console.log(error.message));
-}
+}; 
 
-const populateTitles = (programmes) => {
-    results = [];
-    let resultObj = JSON.parse(programmes);
-    
-    //array of netflix programmes
-    if (Array.isArray(resultObj)) {
-        results = results.concat(resultObj);
+const getGenres = () => {
+Request("GET", getAllGenres).then(response => {
+    populateGenres(response);
+    }).catch(error => console.log(error.message));
+}; 
+
+const populateTitles = programmes => {
+    populateDropDown(programmes, "netflixProgram", "netflixId", "title");
+
+
+};
+                                 
+const populateGenres = genres => {
+    populateDropDown(genres, "genre", "genreId", "genre);
+};  
+
+const populateDropDown = (response, drpdwn, value, field) => {
+    let results = [];
+    resultObj = JSON.parse(response);
+    //add to array
+     (Array.isArray(resultObj)) {
+    lts = results.concat(resultObj);
     } else {
-        results.push(resultObj);
-    }
+    results.push(resultObj);
+    }; 
+    
+
 
     //select dropdown
-    let dropdown = document.getElementById("netflixProgram");
-
+    let dropdown = document.getElementById(drpdwn);
+    
     //populate dropdown
-    results.forEach(value => {
+    results.forEach(item => {
         let option = document.createElement('option');
-        option.value = value.netflixId;
-        option.innerHTML = value.title;
+        option.value = item.value;
+        option.innerHTML = item.field;
         dropdown.appendChild(option);
-    })
+    });
 
 
 };
